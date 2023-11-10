d78c6d390e791837ace5e531a2787a902d216f86
maxilanger <149794442+maxilanger@users.noreply.github.com>
2023-11-10 13:16:22 +0100

    fix type in exercise-002

--- exercise-002/main.cpp
+++ exercise-002/main.cpp
@@ -1,11 +1,14 @@
 #include <fmt/chrono.h>
 #include <fmt/format.h>
-
+#include <vector>
+#include <random>
+#include <iostream>
 #include "CLI/CLI.hpp"
 #include "config.h"
 
 auto main(int argc, char **argv) -> int
 {
+    auto count = 20;
     /**
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
@@ -14,21 +17,32 @@ auto main(int argc, char **argv) -> int
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
+        app.add_option("-c,--count", count, fmt::format("Create a vector with the given size default: {}", count));
         app.parse(argc, argv);
     }
     catch (const CLI::ParseError &e)
     {
         return app.exit(e);
     }
-
-    /**
-     * The {fmt} lib is a cross platform library for printing and formatting text
+    
+     std::vector <int> v(count);
+     std::random_device rd;
+     std::mt19937 gen(rd());
+     std::uniform_int_distribution<> verteilung(1, 100);
+     for (int i=0; i < 20; ++i){
+        v[i] = verteilung(gen);
+     }
+     std::cout << "Mein Vektor mit zufälligen Zahlen: ";
+     for (int i =0; i < 20; ++i) {
+        std::cout << v[i] << " ";
+     }
+     /* The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
     fmt::print("Hello, {}!\n", app.get_name());
 
     /* INSERT YOUR CODE HERE */
-
+    
     return 0; /* exit gracefully*/
 }
